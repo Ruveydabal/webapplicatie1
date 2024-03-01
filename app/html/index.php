c37c447d101028efda2ea224672d7ee06b1c2548
Ruveydabal <ruveydabal024@gmail.com>
2024-03-01 12:13:05 +0100

    eerste keer

---
+++ app/html/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <!-- hier begint de nav -->
+    <main>
+        <section nav>
+            <div class="nav-balk">
+                <div class="blok-logo">
+                    <a class="tekst">Logo</a>
+                </div>
+                <div class="blok-home">
+                    <a class="tekst2">Home</a>
+                </div>
+                <div class="blok-my-books">
+                    <a class="tekst2">My Books</a>
+                </div>
+                <div class="blok-browse">
+                    <a class="tekst2">Browse</a>
+                </div>
+                <div class="blok-zoeken">
+                    <div class="balk">
+                        <a class="tekst3">Search Books</a>
+                    </div>
+                </div>
+                <div class="blok-inlog">
+                    <a class="tekst2">Inlog</a>
+                </div>
+            </div>
+        </section>
+
+        <section currently reading>
+            <div class="blok-links">
+                <div class="row">
+                    <a class="tekst4">currently reading</a>
+                </div>
+                <div class="blok-boek">
+                    <div class="boek-box">
+                        
+                    </div>
+                    
+                </div>
+            </div>
+        </section>    
+
+    <main>
+<footer>
+
+</footer>
+</body>
+</html>
